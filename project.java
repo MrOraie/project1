0e4961126e6db433830c6ad8363fb50953d09325
oraie <mohammadreza.oraie@email.kntu.ac.ir>
2024-11-14 08:15:10 +0330

    ver13

--- project.java
+++ project.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class project {  
     public static void main(String[] args) {  
         Scanner scanner = new Scanner(System.in);   
-        int number = Integer.parseInt(scanner.nextLine());   
+            System.out.print("Enter a number: ");  
+            int number = Integer.parseInt(scanner.nextLine());   
+            System.out.println("You entered: " + number);  
+            
+
+            scanner.close();  
     }  
 }
